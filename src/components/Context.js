50702409ee89655e1682b1f7db44347611d32e57
nedralovesme <ardenrose@gmail.com>
2017-03-08 09:47:24 -0600

    first push

---
+++ src/components/Context.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class ContextExample extends React.Component {
+  static contextTypes = {
+    authenticated: React.PropTypes.bool,
+    user: React.PropTypes.object
+  };
+
+  render() {
+    if (!this.context.authenticated) {
+      return (
+        <div>
+          You need to <LoginLink />.
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        Welcome {this.context.user.username}!
+      </div>
+    );
+  }
+}
